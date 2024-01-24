8b84d27facaa3c317d35f5eaf406e457a66333d4
Thomas <tgusewelle@gkwmedia.com>
2024-01-24 14:28:24 -0600

    update clock format

--- src/app/status/page.tsx
+++ src/app/status/page.tsx
@@ -1,24 +1,29 @@
-'use client'
+"use client";
 import { webSocketContext } from "@/providers/webSocket";
 import { useContext, useEffect, useState } from "react";
 
 const StatusPage = () => {
   const context = useContext(webSocketContext);
-  const [time, setTime] = useState("")
+  const [time, setTime] = useState("");
 
   useEffect(() => {
     const UpdateTime = () => {
-      setTime(new Date().toLocaleTimeString())
-    }
-    setInterval(UpdateTime)
-  }, [])
+      setTime(
+        new Date().toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        })
+      );
+    };
+    setInterval(UpdateTime);
+  }, []);
   //
   //
   //  useEffect(() => {
   //    console.log(context?.data);
   //
   //  }, [context?.data])
-  // 
+  //
   // if (!context) {
   //   return <></>
   // }
@@ -30,28 +35,31 @@ const StatusPage = () => {
 
   function formatTime(d: Date | undefined) {
     if (d == undefined) {
-      return 
+      return;
     }
-    return d.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+    return d.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
   }
 
-
-  return <div className="min-h-screen flex items-center justify-center">
-
-    {context?.data.type == "NONE" ?
-      <h1 className="text-8xl font-bold">{time}</h1>
-      :
-      <div className="flex flex-col gap-36">
-        <div className="flex flex-col items-center justify center gap-3">
-          <h3 className="text-2xl">{context?.data.type == "ON THE WAY" ? "Preacher is:" : "Preacher"}</h3>
-          <h1 className="text-8xl font-bold">{context?.data.type}</h1>
-          <p className="text-2xl font-semibold">{`at: ${formatTime(context?.data.time)}`}</p>
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      {context?.data.type == "NONE" ? (
+        <h1 className='text-8xl font-bold'>{time}</h1>
+      ) : (
+        <div className='flex flex-col gap-36'>
+          <div className='flex flex-col items-center justify center gap-3'>
+            <h3 className='text-2xl'>
+              {context?.data.type == "ON THE WAY" ? "Preacher is:" : "Preacher"}
+            </h3>
+            <h1 className='text-8xl font-bold'>{context?.data.type}</h1>
+            <p className='text-2xl font-semibold'>{`at: ${formatTime(
+              context?.data.time
+            )}`}</p>
+          </div>
+          <h1 className='text-3xl font-bold text-center'>{time}</h1>
         </div>
-        <h1 className="text-3xl font-bold text-center">{time}</h1>
-      </div>
-
-    }
-  </div>
-}
+      )}
+    </div>
+  );
+};
 
 export default StatusPage;
